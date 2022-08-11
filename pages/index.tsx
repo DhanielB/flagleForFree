a087bccf63299c7e2dca6221f4388d6aa6cbed22
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-08-10 21:36:27 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -311,14 +311,14 @@ export default function Home() {
           className="text-center text-white p-2 border border-gray bg-transparent left-32 outline-none rounded absolute"
           placeholder="Enter a coutry"
           onInput={(event) => {
-            if (id == validId || id == 1) {
+            if (id == validId) {
               setValue(event.currentTarget.value);
             }
           }}
           value={value}
-          autoFocus={id == validId || id == 1}
+          autoFocus={id == validId}
         ></input>
-        {id == validId || id == 1 ? (
+        {id == validId ? (
           <button
             className="font-bold p-2 bg-white rounded left-80 absolute"
             onClick={() => {
